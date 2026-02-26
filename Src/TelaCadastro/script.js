bf251461c0280c7a1ba371b3e75131a5f4944e5c
Arthur Lobo <arthur.e.a.lobo@gmail.com>
2026-02-26 11:07:48 -0300

    update

--- Src/TelaCadastro/script.js
+++ Src/TelaCadastro/script.js
@@ -35,6 +35,6 @@ document.addEventListener("DOMContentLoaded", () => {
         alert("Cadastro realizado com sucesso!");
 
         // Redirecionamento após cadastro
-        window.location.href = "./Acesse a sua conta/Acesseasuaconta.html";
+        window.location.href = "../Acesse a sua conta/Acesseasuaconta.html";
     });
 });
